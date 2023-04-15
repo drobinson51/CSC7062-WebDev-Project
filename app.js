db0281343536cbc5bcba3b9cbd617987954a0e31
unknown <drobinson51@qub.ac.uk>
2023-04-15 20:23:29 +0100

    Changes made to structure, transfer to ejs files plus work done to allow session authentication, checking DB for info and inserting DB info

--- app.js
+++ app.js
@@ -1,9 +1,111 @@
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+const mysql = require("mysql");
 
-app.listen(3000, () => {
-    console.log("Server is running at port 3000");
+const cookieParser = require("cookie-parser");
+const sessions = require("express-session");
+
+const oneHour = 1000 * 60 * 60 * 1;
+
+app.set("view engine", "ejs");
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+app.use( sessions({
+    secret: "myshows14385899",
+    saveUninitialized: true,
+    cookie: { maxAge: oneHour },
+    resave: false,
+  })
+);
+
+
+
+let db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "7062",
+  port: "3306",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+});
+
+
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.post("/", (req, res) => {
+  let useremail = req.body.emailString;
+  let userpassword = req.body.passwordString;
+
+  let checkuser = "SELECT * FROM my_users WHERE email = ? AND password = ?";
+
+  db.query(checkuser, [useremail, userpassword], (err, rows) => {
+    if (err) throw err;
+    let numRows = rows.length;
+
+    if (numRows > 0) {
+      let sessionobj = req.session;
+      sessionobj.authen = rows[0].id;
+      res.redirect('/home');
+    } else {
+      res.redirect('/')
+    }
   });
-  
+});
+
+
+app.get('/home', (req,res) => {
+  let sessionobj = req.session;
+  if(sessionobj.authen){
+    let userid = sessionobj.authen;
+    let user = "SELECT * FROM my_users WHERE id = ?";
+
+    db.query(user, [userid], (err, row) => {
+      let firstrow = row[0];
+      res.render('home', {userdata:firstrow});
+    });
+      
+  }else{
+      res.send("Access has been denied");
+  } 
+});
+
+app.get('/admin/add', (req, res) => {
+  res.render("addrecord");
+});
+
+app.post('/admin/add', (req, res) => {
+    let albumname = req.body.albumField;
+    let artistname = req.body.artistField;
+    // let albumdesc = req.body.descField;
+    let albumdesc = "Troubleshooting as we speak";
+    // res.send(`You have not added ${albumname}, ${artistname}, ${albumdesc}`);
+    let albumsql = "INSERT INTO my_albums (albumname, albumartist, albumdesc) VALUES( ? , ? , ?)";
+    db.query(albumsql,[albumname, artistname, albumdesc],(err, rows)=>{
+      if(err) throw err;
+      res.send(`You have added: <p>${albumname}</p> <p>${artistname}</p> <p>${albumdesc}</p>`);
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running at port 3000");
+});
+
+
+
+
+
+// app.listen(3000, () => {
+//   console.log("Server on port 3000");
+// });
